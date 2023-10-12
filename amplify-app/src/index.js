7765ab09f1dcc3a2827553ac0733bb8d3383db48
Erling Holte <erling.holte@outlook.com>
2023-10-12 19:28:53 +0200

    Finally fixed old imports

--- amplify-app/src/index.js
+++ amplify-app/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,11 +9,12 @@ import { Amplify } from 'aws-amplify'
 import awsconfig from './aws-exports'
 Amplify.configure(awsconfig) // Configures the Amplify libraries with the cloud backend set up via the Amplify CLI
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = document.getElementById('root'); // <- This is the correct method call for React version 17
+render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  root
 );
 
 // If you want to start measuring performance in your app, pass a function
