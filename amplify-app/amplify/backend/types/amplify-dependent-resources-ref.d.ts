c5368f618d14942b89946ff5f75b09824f874ade
Erling Holte <erling.holte@outlook.com>
2023-10-12 18:44:45 +0200

    Connect API

--- amplify-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,20 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "amplifyappe7254c4f": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- amplify-app/src/App.js
+++ amplify-app/src/App.js
@@ -1,102 +1,56 @@
-import { Component } from 'react';
-import Amplify, { API, graphqlOperation } from 'aws-amplify';
-import { createNote, deleteNote } from './graphql/mutations';
-import { listNotes } from './graphql/queries';
+import './App.css';
+import { createNote, deleteNote} from './graphql/mutations'
+import { listNotes } from './graphql/queries'
+import { withAuthenticator, Button, Text, Flex, Heading } from "@aws-amplify/ui-react";
+import { useCallback, useEffect, useState } from 'react';
+import { API } from 'aws-amplify';
 
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-import awsExports from './aws-exports';
+function App({ signOut }) {
+  const [ notes, setNotes ] = useState([])
 
-Amplify.configure(awsExports);
+  const fetchNotes = useCallback(async () => {
+    const result = await API.graphql({
+      query: listNotes,
+      authMode: 'AMAZON_COGNITO_USER_POOLS'
+    })
+    setNotes(result.data.listNotes.items)
+  }, [setNotes])
 
-class AddNote extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { text: '' };
-  }
+  const handleCreateNote = useCallback(async () => {
+    await API.graphql({
+      query: createNote,
+      variables: { input: { text: window.prompt("New note") } },
+      authMode: 'AMAZON_COGNITO_USER_POOLS'
+    })
+    fetchNotes()
+  }, [fetchNotes])
 
-  handleChange = (event) => {
-    this.setState({ text: event.target.value });
-  }
+  const handleDeleteNote = useCallback(async (id) => {
+    await API.graphql({
+      query: deleteNote,
+      variables: { input: { id: id } },
+      authMode: 'AMAZON_COGNITO_USER_POOLS'
+    })
+    fetchNotes()
+  }, [fetchNotes])
 
-  handleClick = () => {
-    this.props.addNote(this.state);
-    this.setState({ text: '' });
-  }
+  useEffect(() => {
+    fetchNotes()
+  }, [fetchNotes])
 
-  render() {
-    return (
-      <div style={styles.form}>
-        <input
-          value={this.state.text}
-          onChange={this.handleChange}
-          placeholder="New Note"
-          style={styles.input}
-        />
-        <button onClick={this.handleClick} style={styles.addButton}>Add Note</button>
-      </div>
-    );
-  }
-}
-
-class NotesList extends Component {
-  render() {
-    return (
-      <div>
-        {this.props.notes.map(note =>
-          <div key={note.id} style={styles.note}>
-            <p>{note.text}</p>
-            <button onClick={() => { this.props.deleteNote(note) }} style={styles.deleteButton}>x</button>
-          </div>
-        )}
-      </div>
-    );
-  }
-}
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { notes: [] };
-  }
-
-  async componentDidMount() {
-    var result = await API.graphql(graphqlOperation(listNotes));
-    this.setState({ notes: result.data.listNotes.items });
-  }
-
-  deleteNote = async (note) => {
-    const id = {
-      id: note.id
-    };
-    await API.graphql(graphqlOperation(deleteNote, { input: id }));
-    this.setState({ notes: this.state.notes.filter(item => item.id !== note.id) });
-  }
-
-  addNote = async (note) => {
-    var result = await API.graphql(graphqlOperation(createNote, { input: note }));
-    this.state.notes.push(result.data.createNote);
-    this.setState({ notes: this.state.notes });
-  }
-
-  render() {
-    return (
-      <div style={styles.container}>
-        <h1>Notes App</h1>
-        <AddNote addNote={this.addNote} />
-        <NotesList notes={this.state.notes} deleteNote={this.deleteNote} />
-        <AmplifySignOut />
-      </div>
-    );
-  }
+  return (
+    <Flex direction={"column"}>
+      <Flex justifyContent={'space-between'}>
+        <Heading level={1}>My notes</Heading>
+        <Button onClick={signOut}>Sign Out</Button>
+      </Flex>
+      {notes.map(note => <Flex alignItems={'center'}>
+        <Text>{note.text}</Text>
+        <Button onClick={() => handleDeleteNote(note.id)}>Remove</Button>
+      </Flex>)}
+      <Button onClick={handleCreateNote}>Add Note</Button>
+    </Flex>
+  );
 }
 
 export default withAuthenticator(App);
-
-const styles = {
-  container: { width: 480, margin: '0 auto', padding: 20 },
-  form: { display: 'flex', marginBottom: 15 },
-  input: { flexGrow: 2, border: 'none', backgroundColor: '#ddd', padding: 12, fontSize: 18 },
-  addButton: { backgroundColor: 'black', color: 'white', outline: 'none', padding: 12, fontSize: 18 },
-  note: { display: 'flex', justifyContent: 'space-between', alignItems: 'center', fontSize: 22, marginBottom: 15 },
-  deleteButton: { fontSize: 18, fontWeight: 'bold' }
-}
